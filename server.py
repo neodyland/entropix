05d90e2a33c7ca5273ba582e718de231d71703d0
Googlefan <google@neody.dev>
2024-10-14 15:42:16 +0000

    feat: Add FastAPI server for chat completions

---
+++ server.py
@@ -0,0 +1,170 @@
+from pydantic import TypeAdapter
+from transformers import (
+    AutoTokenizer,
+    AutoModelForCausalLM,
+    AutoConfig,
+    PretrainedConfig,
+)
+import torch
+import json
+from entropixing.generate import generate, stream
+from asyncio import Lock
+from uvicorn import run
+from fastapi import FastAPI, Response, Request
+from fastapi.responses import StreamingResponse
+from openai.types.model import Model
+from openai.types.chat import ChatCompletionChunk, ChatCompletion, ChatCompletionMessage
+from openai.types.chat.chat_completion_chunk import Choice, ChoiceDelta
+from openai.types.chat.chat_completion import Choice as NostreamChoice
+from openai.types.chat.completion_create_params import CompletionCreateParams
+from uuid import uuid4
+import time
+
+if torch.backends.mps.is_available():
+    device = torch.device("mps")
+elif torch.cuda.is_available():
+    device = torch.device("cuda")
+else:
+    device = torch.device("cpu")
+
+print(f"Default device: {device}")
+
+torch.set_float32_matmul_precision("high")
+adapter: TypeAdapter[CompletionCreateParams] = TypeAdapter(CompletionCreateParams)
+
+
+def main():
+    from argparse import ArgumentParser
+
+    global dtype
+    global device
+    global weights
+    global tokenizer
+    global lock
+    lock = Lock()
+    parser = ArgumentParser()
+    parser.add_argument(
+        "--model", type=str, required=True, default="google/gemma-2-2b-jpn-it"
+    )
+    parser.add_argument(
+        "--dtype",
+        type=str,
+        choices=["float16", "bfloat16", "float32"],
+        default="bfloat16",
+    )
+    parser.add_argument("--device", type=str, default=device.type)
+    parser.add_argument("--port", type=int, default=8000)
+    parser.add_argument("--host", type=str, default="0.0.0.0")
+    args = parser.parse_args()
+    device = torch.device(args.device)
+    print(f"Using device: {device}")
+    arch: PretrainedConfig = AutoConfig.from_pretrained(args.model)
+    if arch.architectures[0] not in [
+        "Gemma2ForCausalLM",
+        "LlamaForCausalLM",
+        "Qwen2ForCausalLM",
+    ]:
+        raise ValueError(f"Unsupported model architecture: {arch.architectures[0]}")
+    dtype = getattr(torch, args.dtype)
+    weights = AutoModelForCausalLM.from_pretrained(
+        args.model,
+        device_map=device,
+        torch_dtype=dtype,
+    ).eval()
+    tokenizer = AutoTokenizer.from_pretrained(args.model)
+    app = FastAPI()
+
+    @app.post("/chat/completions")
+    async def chat_completion(body: Request) -> Response:
+        j = adapter.validate_python(await body.json())
+        max_length = j.get("max_completion_tokens") or 512
+        messages = list(j["messages"])
+        if j.get("stream"):
+
+            async def stream_generator():
+                async for chunk in gen(messages, max_length):
+                    yield f"data: {ChatCompletionChunk(
+                        id=str(uuid4()),
+                        choices=[Choice(delta=ChoiceDelta(content=chunk), index=0)],
+                        created=time.time() // 1000,
+                        model=j["model"],
+                        object="chat.completion.chunk",
+                    ).model_dump_json()}\n\n"
+                yield f"data: {ChatCompletionChunk(
+                    id=str(uuid4()),
+                    choices=[
+                        Choice(delta=ChoiceDelta(), finish_reason="stop", index=0)
+                    ],
+                    created=time.time() // 1000,
+                    model=j["model"],
+                    object="chat.completion.chunk",
+                ).model_dump_json()}\n\n"
+
+            return StreamingResponse(
+                content=stream_generator(), media_type="text/event-stream"
+            )
+        else:
+            text = await gen_no_stream(messages, max_length)
+            return Response(
+                content=ChatCompletion(
+                    id=str(uuid4()),
+                    choices=[
+                        NostreamChoice(
+                            finish_reason="stop",
+                            message=ChatCompletionMessage(
+                                content=text, role="assistant"
+                            ),
+                            index=0,
+                        )
+                    ],
+                    created=time.time() // 1000,
+                    model=j["model"],
+                    object="chat.completion",
+                ).model_dump_json(),
+                media_type="application/json",
+            )
+
+    @app.get("/models")
+    async def models():
+        return Response(
+            content=json.dumps(
+                {
+                    "data": [
+                        json.loads(
+                            Model(
+                                id="entropix-any",
+                                object="model",
+                                created=1,
+                                owned_by="someone",
+                            ).model_dump_json()
+                        )
+                    ]
+                }
+            ),
+            media_type="application/json",
+        )
+
+    run(app, host=args.host, port=args.port)
+
+
+async def gen_no_stream(conv, max_length):
+    text = ""
+    async for chunk in gen(conv, max_length):
+        text += chunk
+    return text
+
+
+async def gen(conv, max_length):
+    inputs = tokenizer.apply_chat_template(
+        conv, return_tensors="pt", add_generation_prompt=True
+    )
+    async with lock:
+        it = generate(
+            weights, inputs, device, dtype, [tokenizer.eos_token_id], max_length
+        )
+        for token in stream(it, tokenizer):
+            yield token
+
+
+if __name__ == "__main__":
+    main()
