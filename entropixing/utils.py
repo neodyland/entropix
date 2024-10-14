86d19dfb26e3cbdff8a15dd9bf080b178ff1f5e8
Googlefan <google@neody.dev>
2024-10-14 06:24:15 +0000

    feat(entropixing): add utility functions for frequency scaling and precomputation

---
+++ entropixing/utils.py
@@ -0,0 +1,77 @@
+import torch
+
+
+def apply_scaling(freqs: torch.Tensor) -> torch.Tensor:
+    SCALE_FACTOR = 8.0
+    LOW_FREQ_FACTOR = 1.0
+    HIGH_FREQ_FACTOR = 4.0
+    OLD_CONTEXT_LEN = 8192  # original llama3 length
+
+    low_freq_wavelen = OLD_CONTEXT_LEN / LOW_FREQ_FACTOR
+    high_freq_wavelen = OLD_CONTEXT_LEN / HIGH_FREQ_FACTOR
+
+    def scale_freq(freq: torch.Tensor) -> torch.Tensor:
+        wavelen = 2 * torch.pi / freq
+
+        # Calculate smooth factor
+        smooth = (OLD_CONTEXT_LEN / wavelen - LOW_FREQ_FACTOR) / (
+            HIGH_FREQ_FACTOR - LOW_FREQ_FACTOR
+        )
+        smooth = torch.clamp(smooth, 0.0, 1.0)  # Ensure smooth is between 0 and 1
+
+        # Calculate scaled frequency
+        scaled = (1 - smooth) * freq / SCALE_FACTOR + smooth * freq
+
+        # Apply conditional scaling
+        scaled = torch.where(
+            wavelen < high_freq_wavelen,
+            freq,  # No scaling
+            torch.where(
+                wavelen > low_freq_wavelen,
+                freq / SCALE_FACTOR,  # Apply scaling factor
+                scaled,  # Apply smooth scaling
+            ),
+        )
+        return scaled
+
+    scaled_freqs = torch.vmap(scale_freq)(freqs)
+
+    return scaled_freqs
+
+
+def precompute_freqs_cis(
+    dim: int,
+    end: int,
+    theta: float = 500000.0,
+    use_scaled: bool = False,
+    dtype: torch.dtype = torch.float32,
+    device: torch.device = "cpu",
+) -> torch.Tensor:
+    freqs = 1.0 / (
+        theta
+        ** (torch.arange(0, dim, 2, dtype=dtype, device=device)[: (dim // 2)] / dim)
+    )
+    if use_scaled:
+        freqs = apply_scaling(freqs)
+
+    t = torch.arange(end, dtype=dtype, device=device).unsqueeze(1)  # Shape: (end, 1)
+    freqs = freqs.unsqueeze(0)  # Shape: (1, dim//2)
+    freqs = t * freqs  # Broadcasting to shape: (end, dim//2)
+    return torch.exp(1j * freqs)
+
+
+def build_attn_mask(
+    seqlen: int,
+    start_pos: int,
+    device: torch.device = "cpu",
+) -> torch.Tensor:
+    mask = None
+    if seqlen > 1:
+        mask = torch.full((seqlen, seqlen), float("-inf"))
+        mask = torch.triu(mask, diagonal=1)
+        mask = (
+            torch.hstack([torch.zeros((seqlen, start_pos)), mask])
+            .to(torch.float32)
+            .to(device)
+        )
+    return mask
